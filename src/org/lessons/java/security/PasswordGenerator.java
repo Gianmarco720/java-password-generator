8b72404265a6dafc21a66ae787918443a5c52191
Gianmarco720 <107807132+Gianmarco720@users.noreply.github.com>
2023-11-29 13:51:39 +0100

    Initial commit

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,57 @@
+package org.lessons.java.security;
+
+import java.util.Scanner;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        // creo una variabile per lo scanner
+        Scanner scan = new Scanner(System.in);
+
+        // chiedo all'utente il nome
+        System.out.println("Inserisci il tuo nome:");
+        String userName = scan.nextLine();
+
+        // chiedo all'utente il cognome
+        System.out.println("Inserisci il tuo cognome:");
+        String userLastName = scan.nextLine();
+
+        // chiedo all'utente il suo colore preferito
+        System.out.println("Inserisci il tuo colore preferito:");
+        String userColor = scan.nextLine();
+
+        // chiedo all'utente la sua data di nascita
+
+        // giorno
+        System.out.println("In che giorno sei nato?");
+        int userDayOfBirth = scan.nextInt();
+
+        // mese
+        System.out.println("In che mese sei nato? (numero)");
+        int userMonthOfBirth = scan.nextInt();
+
+        // anno
+        System.out.println("In che anno sei nato?");
+        int userYearOfBirth = scan.nextInt();
+
+        // data completa
+        String userDateOfBirth = userDayOfBirth + "/" + userMonthOfBirth + "/" + userYearOfBirth;
+
+        // sommo i componenti della data di nascita dell'utente
+        int userDateOfBirthSum = userDayOfBirth + userMonthOfBirth + userYearOfBirth;
+
+        // creo la sua password
+        String password = userName + "-" + userLastName + "-" + userColor + "-" + userDateOfBirthSum;
+
+        // stampo in console le info dell'utente
+        System.out.println("Nome: " + userName);
+        System.out.println("Cognome: " + userLastName);
+        System.out.println("Colore preferito: " + userColor);
+        System.out.println("Data di nascita: " + userDateOfBirth);
+
+        // stampo la password dell'utente in console
+        System.out.println("Ecco la tua insicurissima password: " + password );
+
+        // chiudo lo scanner
+        scan.close();
+    }
+}
